26645031321cf90c81197681b407f703401d4d61
Steven Vroonland <steven.vroonland@ordina.nl>
2020-05-16 11:17:13 +0200

    Implementation of HalfClosed

--- src/main/scala/nl/vroste/rezilience/CircuitBreaker.scala
+++ src/main/scala/nl/vroste/rezilience/CircuitBreaker.scala
@@ -10,7 +10,6 @@ object CircuitBreaker {
   case class WrappedError[E](error: E) extends CircuitBreakerCallError[E]
 
   // TODO how strict on max failures when you fire 200 calls simultaneously?
-  // TODO First open-closed, then half-open
   // TODO reset with exponential backoff
   trait Service {
     def call[R, E, A](f: ZIO[R, E, A]): ZIO[R, CircuitBreakerCallError[E], A]
@@ -22,16 +21,17 @@ object CircuitBreaker {
     onStateChange: State => UIO[Unit] = _ => ZIO.unit
   ): ZManaged[Clock, Nothing, Service] =
     for {
-      state         <- Ref.make[State](Closed).toManaged_
-      nrFailedCalls <- Ref.make[Int](0).toManaged_
-      resetRequests <- ZQueue.bounded[Unit](1).toManaged_
+      state          <- Ref.make[State](Closed).toManaged_
+      halfOpenSwitch <- Ref.make[Boolean](true).toManaged_
+      nrFailedCalls  <- Ref.make[Int](0).toManaged_
+      resetRequests  <- ZQueue.bounded[Unit](1).toManaged_
       _ <- ZStream
             .fromQueue(resetRequests)
             .tap(_ => ZIO(println(s"Got reset request, delaying with ${resetTimeout}")))
             .mapM(_ => ZIO.unit.delay(resetTimeout))
             .tap { _ =>
-              println("Resetting state to closed")
-              state.set(Closed) *> nrFailedCalls.set(0) <* onStateChange(Closed)
+              println("Resetting state to HalfOpen")
+              halfOpenSwitch.set(true) <* state.set(HalfOpen) *> onStateChange(HalfOpen)
             }
             .runDrain
             .forkManaged
@@ -41,28 +41,47 @@ object CircuitBreaker {
           currentState <- state.get
 //          _            = println(s"Current state ${currentState}")
           // TODO reset to HalfOpen state
-          result <- if (currentState == Open) {
-                     ZIO.fail(CircuitBreakerOpen)
-                   } else {
-                     // TODO the state may have changed already after completion of `f`
-                     //noinspection SimplifyTapInspection
-                     f.mapError(WrappedError(_))
-                       .tapError { _ =>
-                         for {
-                           currentNrFailedCalls <- nrFailedCalls.updateAndGet(_ + 1)
-                           _                    = println(s"Nr failed calls is ${currentNrFailedCalls}")
-                           _ <- (state.set(Open) *> resetRequests.offer(()) <* onStateChange(Open))
-                                 .when(currentNrFailedCalls >= maxFailures)
-                         } yield ()
-                       }
-                       .tap(_ => nrFailedCalls.set(0))
+          result <- currentState match {
+                     case Open =>
+                       ZIO.fail(CircuitBreakerOpen)
+                     case HalfOpen =>
+                       for {
+                         letCallsThrough <- halfOpenSwitch.getAndUpdate(_ => false)
+                         result <- if (letCallsThrough) {
+                                    //noinspection SimplifyTapInspection
+                                    f.mapError(WrappedError(_))
+                                      .tapError { _ =>
+                                        state.set(Open) *> resetRequests.offer(()) <* onStateChange(Open)
+                                      }
+                                      .tap { _ =>
+                                        state.set(Closed) *> nrFailedCalls.set(0) <* onStateChange(Closed)
+                                      }
+
+                                  } else {
+                                    ZIO.fail(CircuitBreakerOpen)
+                                  }
+                       } yield result
+
+                     case Closed =>
+                       // TODO the state may have changed already after completion of `f`
+                       //noinspection SimplifyTapInspection
+                       f.mapError(WrappedError(_))
+                         .tapError { _ =>
+                           for {
+                             currentNrFailedCalls <- nrFailedCalls.updateAndGet(_ + 1)
+                             _                    = println(s"Nr failed calls is ${currentNrFailedCalls}")
+                             _ <- (state.set(Open) *> resetRequests.offer(()) <* onStateChange(Open))
+                                   .when(currentNrFailedCalls >= maxFailures)
+                           } yield ()
+                         }
+                         .tap(_ => nrFailedCalls.set(0))
                    }
         } yield result
     }
 
   sealed trait State
-  case object Closed extends State
-//  case object HalfOpen extends State
-  case object Open extends State
+  case object Closed   extends State
+  case object HalfOpen extends State
+  case object Open     extends State
 
 }
