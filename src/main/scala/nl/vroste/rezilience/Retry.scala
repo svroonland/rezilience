7697594c6def3a1380499869a74cec10a62fe7a1
Steven Vroonland <svroonland@gmail.com>
2020-09-20 18:54:23 +0200

    Remove overload

--- src/main/scala/nl/vroste/rezilience/Retry.scala
+++ src/main/scala/nl/vroste/rezilience/Retry.scala
@@ -21,22 +21,6 @@ object Retry {
     Schedule.exponential(min, factor).whileOutput(_ <= max) andThen Schedule.fixed(max).as(max)
 
   /**
-   * Schedule for exponential backoff up to a maximum interval and a maximum number of retries
-   *
-   * @param min Minimum backoff time
-   * @param max Maximum backoff time. When this value is reached, subsequent intervals will be equal to this value.
-   * @param factor Exponential factor. 2 means doubling, 1 is constant, < 1 means decreasing
-   * @param maxRecurs Maximum retries. When this number is exceeded, the schedule will end
-   * @tparam A Schedule input
-   */
-  def exponentialBackoff[A](
-    min: Duration,
-    max: Duration,
-    maxRecurs: Int,
-    factor: Double = 2.0
-  ): Schedule[Clock, A, (Duration, Long)] = exponentialBackoff[A](min, max, factor) && Schedule.recurs(maxRecurs)
-
-  /**
    * Apply the given schedule only when inputs match the partial function
    */
   def whenCase[Env, In, Out](pf: PartialFunction[In, Any])(
