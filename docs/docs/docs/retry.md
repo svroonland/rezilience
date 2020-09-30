1900ff420428048d512902e52e37ab487f68f5bd
Steven Vroonland <svroonland@gmail.com>
2020-09-30 15:03:58 +0200

    Extend common retry strategy + doc

--- docs/docs/docs/retry.md
+++ docs/docs/docs/retry.md
@@ -5,21 +5,32 @@ permalink: docs/retry/
 ---
 
 # Retry
-ZIO already has excellent built-in support for retrying effects on failures using a `Schedule`, there is not much this library can add.
 
-Two helper methods are made available:
+`Retry` is a policy that retries effects on failure
 
-* `Retry.exponentialBackoff`  
+`rezilience` offers some convenience methods for retrying with a common-practice strategy via `Retry.make`:
+
+* By default the first retry is done immediately. With transient / random failures this method gives the highest chance of fast success.
+* After that Retry uses exponential backoff between some minimum and maximum duration. 
+* Jitter is added to prevent spikes of retries.
+* An optional maximum number of retries ensures that retrying does not continue forever.
+
+ZIO already has excellent built-in support for retrying effects on failures using a `Schedule` and `rezilience` is built on top of that. Retry can accept any `ZIO` [`Schedule`](https://zio.dev/docs/datatypes/datatypes_schedule).
+
+For further Schedule composition, the above strategy is also available as a `ZIO` `Schedule` along with some other building blocks in `Retry.Schedules`:
+
+* `Retry.Schedules.common`  
+  The strategy with immediate retry, exponential backoff and jitter as outlined above.
+
+* `Retry.Schedules.exponentialBackoff`  
   Exponential backoff with a maximum delay and an optional maximum number of recurs. When the maximum delay is reached, subsequent delays are the maximum. 
   
-* `Retry.whenCase`  
-  Accepts a partial function and a schedule and will apply the schedule only when the input matches partial function. This is useful to retry only on certain types of failures/exceptions
-  
-For consistency with the other policies and to support combining policies, there is `Retry.make(schedule)`.
+* `Retry.Schedules.whenCase`  
+  Accepts a partial function and a schedule and will apply the schedule only when the input matches partial function. This is useful to retry only on certain types of failures/exceptions.
   
 ## Usage
   
-```scala
+```scala mdoc
 import zio._
 import zio.duration._
 import nl.vroste.rezilience._
@@ -27,8 +38,30 @@ import java.util.concurrent.TimeoutException
 
 val myEffect: ZIO[Any, Exception, Unit] = ???
 
-// Retry exponentially on timeout exceptions
-myEffect.retry(
-  Retry.Schedule.whenCase({ case TimeoutException => })(Retry.Schedule.exponentialBackoff(min = 1.second, max = 1.minute))
+// Create a Retry policy that only retries timeouts
+val retry = Retry.make(
+  Retry.Schedules.whenCase({ case TimeoutException => }) {
+    Retry.Schedules.common(min = 1.second, max = 1.minute)
+  }
 )
+
+retry.use { retryPolicy => 
+  retryPolicy(myEffect)
+}
+```
+
+## Different retry strategies for different errors
+
+By composing ZIO `Schedule`s, you can define different retries for different types of errors:
+
+```scala 
+
+val retry = Retry.make(
+  Retry.Schedules.whenCase({ case TimeoutException => }) {
+    Retry.Schedules.common(min = 1.second, max = 1.minute)
+  } || Retry.Schedules.whenCase({ case UnknownHostException => }) {
+    Retry.Schedules.common(min = 1.day, max = 5.days)
+  }
+)
+
 ```
--- rezilience/shared/src/main/scala/nl/vroste/rezilience/Retry.scala
+++ rezilience/shared/src/main/scala/nl/vroste/rezilience/Retry.scala
@@ -1,6 +1,7 @@
 package nl.vroste.rezilience
 import zio.clock.Clock
 import zio.duration._
+import zio.random.Random
 import zio.{ Schedule, ZIO, ZManaged }
 
 trait Retry[-E] { self =>
@@ -24,7 +25,84 @@ trait Retry[-E] { self =>
 }
 
 object Retry {
-  object Schedule {
+
+  /**
+   * Create a Retry policy with a common retry schedule
+   *
+   * By default the first retry is done immediately. With transient / random failures this method gives the
+   * highest chance of fast success.
+   * After that Retry uses exponential backoff between some minimum and maximum duration. Jitter is added
+   * to prevent spikes of retries.
+   * An optional maximum number of retries ensures that retrying does not continue forever.
+   *
+   * @param min Minimum retry backoff delay
+   * @param max Maximum backoff time. When this value is reached, subsequent intervals will be equal to this value.
+   * @param factor Factor with which delays increase
+   * @param retryImmediately Retry immediately after the first failure
+   * @param maxRetries Maximum number of retries
+   */
+  def make(
+    min: Duration = 1.second,
+    max: Duration = 1.minute,
+    factor: Double = 2.0,
+    retryImmediately: Boolean = true,
+    maxRetries: Option[Int] = Some(3)
+  ): ZManaged[Clock with Random, Nothing, Retry[Any]] =
+    make(Schedules.common(min, max, factor, retryImmediately, maxRetries))
+
+  /**
+   * Create a Retry from a ZIO Schedule
+   */
+  def make[R, E](schedule: Schedule[R, E, Any]): ZManaged[Clock with R, Nothing, Retry[E]] =
+    ZManaged.environment[Clock with R].map(RetryImpl(_, schedule))
+
+  private case class RetryImpl[-E, ScheduleEnv](
+    scheduleEnv: Clock with ScheduleEnv,
+    schedule: Schedule[ScheduleEnv, E, Any]
+  ) extends Retry[E] {
+    override def apply[R, E1 <: E, A](f: ZIO[R, E1, A]): ZIO[R, E1, A] =
+      ZIO.environment[R].flatMap(env => f.provide(env).retry(schedule).provide(scheduleEnv))
+
+    override def widen[E2](pf: PartialFunction[E2, E]): Retry[E2] = RetryImpl[E2, ScheduleEnv](
+      scheduleEnv,
+      (zio.Schedule.stop ||| schedule).contramap[ScheduleEnv, E2] { e2 =>
+        pf.andThen(Right.apply[E2, E](_)).applyOrElse(e2, Left.apply[E2, E](_))
+      }
+    )
+  }
+
+  /**
+   * Convenience methods to create common ZIO schedules for retrying
+   */
+  object Schedules {
+
+    /**
+     * A common-practice schedule for retrying
+     *
+     * By default the first retry is done immediately. With transient / random failures this method gives the
+     * highest chance of fast success.
+     * After that Retry uses exponential backoff between some minimum and maximum duration. Jitter is added
+     * to prevent spikes of retries.
+     * An optional maximum number of retries ensures that retrying does not continue forever.
+     *
+     * See also https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+     *
+     * @param min Minimum retry backoff delay
+     * @param max Maximum backoff time. When this value is reached, subsequent intervals will be equal to this value.
+     * @param factor Factor with which delays increase
+     * @param retryImmediately Retry immediately after the first failure
+     * @param maxRetries Maximum number of retries
+     */
+    def common(
+      min: Duration = 1.second,
+      max: Duration = 1.minute,
+      factor: Double = 2.0,
+      retryImmediately: Boolean = true,
+      maxRetries: Option[Int] = Some(3)
+    ): Schedule[Any with Random, Any, (Any, Long)] =
+      ((if (retryImmediately) zio.Schedule.once else zio.Schedule.stop) andThen
+        exponentialBackoff(min, max, factor).jittered) &&
+        maxRetries.fold(zio.Schedule.forever)(zio.Schedule.recurs)
 
     /**
      * Schedule for exponential backoff up to a maximum interval
@@ -48,45 +126,5 @@ object Retry {
       schedule: Schedule[Env, In, Out]
     ): Schedule[Env, In, (In, Out)] =
       zio.Schedule.recurWhile(pf.isDefinedAt) && schedule
-  }
-
-  /**
-   * Create a Retry from a ZIO Schedule
-   * @param schedule
-   * @tparam R
-   * @tparam E
-   * @return
-   */
-  def make[R, E](schedule: Schedule[R, E, Any]): ZManaged[Clock with R, Nothing, Retry[E]] =
-    ZManaged.environment[Clock with R].map(RetryImpl(_, schedule))
-
-  /**
-   * Create a Retry policy with exponential backoff
-   *
-   * @param min Minimum retry backoff delay
-   * @param max Maximum retry backoff delay
-   * @param factor Factor with which delays increase
-   * @return
-   */
-  def make(
-    min: Duration = 1.second,
-    max: Duration = 1.minute,
-    factor: Double = 2.0
-  ): ZManaged[Clock, Nothing, Retry[Any]] =
-    ZManaged.environment[Clock].map(RetryImpl(_, Schedule.exponentialBackoff(min, max, factor)))
-
-  private case class RetryImpl[-E, ScheduleEnv](
-    scheduleEnv: Clock with ScheduleEnv,
-    schedule: Schedule[ScheduleEnv, E, Any]
-  ) extends Retry[E] {
-    override def apply[R, E1 <: E, A](f: ZIO[R, E1, A]): ZIO[R, E1, A] =
-      ZIO.environment[R].flatMap(env => f.provide(env).retry(schedule).provide(scheduleEnv))
-
-    override def widen[E2](pf: PartialFunction[E2, E]): Retry[E2] = RetryImpl[E2, ScheduleEnv](
-      scheduleEnv,
-      (zio.Schedule.stop ||| schedule).contramap[ScheduleEnv, E2] { e2 =>
-        pf.andThen(Right.apply[E2, E](_)).applyOrElse(e2, Left.apply[E2, E](_))
-      }
-    )
   }
 }
