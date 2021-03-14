0000b0491b48604d1108da65ee56c8da30c1a02b
Steven Vroonland <steven@dataworkz.nl>
2021-03-14 16:07:14 +0100

    Tests for interruption

--- rezilience/shared/src/test/scala/nl/vroste/rezilience/RateLimiterSpec.scala
+++ rezilience/shared/src/test/scala/nl/vroste/rezilience/RateLimiterSpec.scala
@@ -1,10 +1,12 @@
 package nl.vroste.rezilience
-import zio.{ clock, Promise, ZIO }
+import zio.{ clock, Promise, Ref, ZIO }
 import zio.duration._
 import zio.test._
 import zio.test.Assertion._
-import zio.test.TestAspect.{ nonFlaky, timeout }
+import zio.test.TestAspect.{ diagnose, nonFlaky, timeout }
 import zio.test.environment.TestClock
+
+import java.time.Instant
 
 object RateLimiterSpec extends DefaultRunnableSpec {
   override def spec = suite("RateLimiter")(
@@ -62,7 +64,33 @@ object RateLimiterSpec extends DefaultRunnableSpec {
           _           <- interrupted.await
         } yield assertCompletes
       }
+    },
+    testM("will not start execution of an effect when it is interrupted before getting its turn to execute") {
+      RateLimiter.make(1, 1.second).use { rl =>
+        for {
+          count        <- Ref.make(0)
+          _            <- rl(ZIO.unit)
+          fib          <- rl(count.set(1)).fork
+          interruption <- fib.interrupt.fork
+          _            <- interruption.join
+          c            <- count.get
+        } yield assert(c)(equalTo(0))
+      }
+    },
+    testM("will make effects wait for interrupted effects to pass through the rate limiter") {
+      RateLimiter.make(1, 1.second).use { rl =>
+        for {
+          _  <- rl(ZIO.unit) // Execute one
+          f1 <- rl(ZIO.unit).fork
+          _  <- TestClock.adjust(1.second) // This ensures that the second RL call is in the stream before we interrupt
+          _  <- f1.interrupt
 
+          // This one will have to wait 1 seconds
+          fib               <- rl(clock.instant).fork
+          _                 <- TestClock.adjust(1.second)
+          lastExecutionTime <- fib.join
+        } yield assert(lastExecutionTime)(equalTo(Instant.ofEpochSecond(2)))
+      }
     }
-  ) @@ nonFlaky @@ timeout(60.seconds)
+  ) @@ timeout(10.seconds) @@ diagnose(10.seconds) @@ nonFlaky
 }
