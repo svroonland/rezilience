44c746618b8faaaf063fc729a09b151f18cf9435
Steven Vroonland <steven@dataworkz.nl>
2021-04-19 20:50:17 +0200

    Cleanup

--- rezilience/shared/src/main/scala/nl/vroste/rezilience/SwitchablePolicy.scala
+++ rezilience/shared/src/main/scala/nl/vroste/rezilience/SwitchablePolicy.scala
@@ -26,26 +26,33 @@ object SwitchablePolicy {
   /**
    * Creates a Policy that can be replaced safely at runtime
    */
-  def make[R0, E0, E](initial: ZManaged[R0, E0, Policy[E]]): ZManaged[R0, E0, SwitchablePolicy[R0, E0, E]] = {
-    def makeInUsePolicyState(
-      scope: ZManaged.Scope,
-      newPolicy: ZManaged[R0, E0, Policy[E]]
-    ): ZIO[R0, E0, InUsePolicyState[E]] = for {
-      newDone            <- Promise.make[Nothing, Unit]
-      newInUse           <- TRef.make(0L).commit
-      newShutdownBegan   <- TRef.make(false).commit
-      r                  <- scope.apply(newPolicy)
-      (finalizer, policy) = r
-    } yield InUsePolicyState[E](policy, finalizer, newInUse, newShutdownBegan, newDone)
-
+  def make[R0, E0, E](initial: ZManaged[R0, E0, Policy[E]]): ZManaged[R0, E0, SwitchablePolicy[R0, E0, E]] =
     for {
       scope         <- ZManaged.scope
       policyState   <- makeInUsePolicyState(scope, initial).toManaged_
-      currentPolicy <- STM.atomically(TRef.make(policyState)).toManaged_
+      currentPolicy <- TRef.make(policyState).commit.toManaged_
     } yield new SwitchablePolicy[R0, E0, E] {
       override def apply[R, E1 <: E, A](f: ZIO[R, E1, A]): ZIO[R, PolicyError[E1], A] =
         ZManaged.make(beginCallWithPolicy)(endCallWithPolicy).map(_.policy).use { policy =>
           policy.apply(f)
+        }
+
+      def beginCallWithPolicy: IO[Nothing, InUsePolicyState[E]] = STM.atomically {
+        for {
+          currentState <- currentPolicy.get
+          _            <- currentState.inFlightCalls.update(_ + 1)
+        } yield currentState
+      }
+
+      def endCallWithPolicy(currentState: InUsePolicyState[E]): IO[Nothing, Unit] =
+        STM.atomically {
+          for {
+            newInUse      <- currentState.inFlightCalls.updateAndGet(_ - 1)
+            shutdownBegan <- currentState.shuttingDown.get
+            done           = shutdownBegan && (newInUse == 0)
+          } yield done
+        }.flatMap { done =>
+          ZIO.when(done)(currentState.shutdownComplete.succeed(()))
         }
 
       override def switch(newPolicy: ZManaged[R0, E0, Policy[E]]): ZIO[R0, E0, UIO[Unit]] =
@@ -61,40 +68,31 @@ object SwitchablePolicy {
                                 }.onInterrupt(newPolicyState.finalizer.apply(Exit.unit))
           // From this point on, new policy calls will use the new policy
           // Use a promise to decouple the 'await' effect from the fiber running the finalizer
-          policyReleased     <- Promise.make[Nothing, Unit]
-          complete           <-
+          policyReleased     <- Promise.make[Nothing, Any]
+          _                  <-
             (currentPolicyState.shutdownComplete.await *> currentPolicyState.finalizer
               .apply(Exit.unit)
-              .to(policyReleased)).unit.fork
-        } yield policyReleased.await
-
-      private def beginCallWithPolicy: IO[Nothing, InUsePolicyState[E]] = STM.atomically {
-        for {
-          currentState <- currentPolicy.get
-          _            <- currentState.inUse.update(_ + 1)
-        } yield currentState
-      }
-
-      private def endCallWithPolicy(currentState: InUsePolicyState[E]): IO[Nothing, Unit] =
-        STM.atomically {
-          for {
-            newInUse      <- currentState.inUse.updateAndGet(_ - 1)
-            shutdownBegan <- currentState.shuttingDown.get
-            done           = shutdownBegan && (newInUse == 0)
-          } yield done
-        }.flatMap { done =>
-          ZIO.when(done)(currentState.shutdownComplete.succeed(()))
-        }
-
+              .to(policyReleased)).fork
+        } yield policyReleased.await.unit
     }
-  }
 
   private case class InUsePolicyState[E](
     policy: Policy[E],
     finalizer: ZManaged.Finalizer,
-    inUse: TRef[Long],
+    inFlightCalls: TRef[Long],
     shuttingDown: TRef[Boolean],
     shutdownComplete: Promise[Nothing, Unit]
   )
+
+  private def makeInUsePolicyState[R0, E0, E](
+    scope: ZManaged.Scope,
+    newPolicy: ZManaged[R0, E0, Policy[E]]
+  ): ZIO[R0, E0, InUsePolicyState[E]] = for {
+    shutdownComplete   <- Promise.make[Nothing, Unit]
+    inFlightCalls      <- TRef.make(0L).commit
+    shuttingDown       <- TRef.make(false).commit
+    r                  <- scope.apply(newPolicy)
+    (finalizer, policy) = r
+  } yield InUsePolicyState(policy, finalizer, inFlightCalls, shuttingDown, shutdownComplete)
 
 }
