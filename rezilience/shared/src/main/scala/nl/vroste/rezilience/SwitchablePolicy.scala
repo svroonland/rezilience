1fd7a75279c222bb03e699d4a998bcf5f6c7b478
Steven Vroonland <steven@dataworkz.nl>
2021-08-01 19:51:32 +0200

    Draft of 'finish in flight' switchable policy mode

--- rezilience/shared/src/main/scala/nl/vroste/rezilience/SwitchablePolicy.scala
+++ rezilience/shared/src/main/scala/nl/vroste/rezilience/SwitchablePolicy.scala
@@ -1,6 +1,7 @@
 package nl.vroste.rezilience
 
 import nl.vroste.rezilience.Policy.PolicyError
+import nl.vroste.rezilience.SwitchablePolicy.Mode
 import zio.stm.{ STM, TRef }
 import zio.{ Exit, IO, Promise, UIO, ZIO, ZManaged }
 
@@ -18,18 +19,26 @@ trait SwitchablePolicy[R0, E0, E] extends Policy[E] {
    * The old policy will be released after those in-flight calls are completed.
    * The inner UIO signals completion of release of the old policy.
    */
-  def switch(newPolicy: ZManaged[R0, E0, Policy[E]]): ZIO[R0, E0, UIO[Unit]]
+  def switch(newPolicy: ZManaged[R0, E0, Policy[E]], mode: Mode = Mode.Transition): ZIO[R0, E0, UIO[Unit]]
 }
 
 object SwitchablePolicy {
 
+  sealed trait Mode
+  object Mode {
+    case object Transition     extends Mode
+    case object FinishInFlight extends Mode
+  }
+
   /**
    * Creates a Policy that can be replaced safely at runtime
    */
-  def make[R0, E0, E](initial: ZManaged[R0, E0, Policy[E]]): ZManaged[R0, E0, SwitchablePolicy[R0, E0, E]] =
+  def make[R0, E0, E](
+    initial: ZManaged[R0, E0, Policy[E]]
+  ): ZManaged[R0, E0, SwitchablePolicy[R0, E0, E]] =
     for {
       scope         <- ZManaged.scope
-      policyState   <- makeInUsePolicyState(scope, initial).toManaged_
+      policyState   <- makeInUsePolicyState(scope, initial, awaitReady = UIO.unit).toManaged_
       currentPolicy <- TRef.make(policyState).commit.toManaged_
     } yield new SwitchablePolicy[R0, E0, E] {
       override def apply[R, E1 <: E, A](f: ZIO[R, E1, A]): ZIO[R, PolicyError[E1], A] =
@@ -37,62 +46,101 @@ object SwitchablePolicy {
           policy.apply(f)
         }
 
-      def beginCallWithPolicy: IO[Nothing, InUsePolicyState[E]] = STM.atomically {
+      def beginCallWithPolicy: IO[Nothing, PolicyState[E]] = STM.atomically {
         for {
           currentState <- currentPolicy.get
           _            <- currentState.inFlightCalls.update(_ + 1)
         } yield currentState
-      }
+      }.tap(policyState => policyState.awaitReady)
 
-      def endCallWithPolicy(currentState: InUsePolicyState[E]): IO[Nothing, Unit] =
+      def endCallWithPolicy(usedPolicyState: PolicyState[E]): IO[Nothing, Unit] =
         STM.atomically {
           for {
-            newInUse      <- currentState.inFlightCalls.updateAndGet(_ - 1)
-            shutdownBegan <- currentState.shuttingDown.get
-            done           = shutdownBegan && (newInUse == 0)
+            newInFlightCalls <- usedPolicyState.inFlightCalls.updateAndGet(_ - 1)
+            shutdownBegan    <- usedPolicyState.shuttingDown.get
+            done              = shutdownBegan && (newInFlightCalls == 0)
           } yield done
         }.flatMap { done =>
-          ZIO.when(done)(currentState.shutdownComplete.succeed(()))
+          ZIO.when(done)(usedPolicyState.shutdownComplete.succeed(()))
         }
 
-      override def switch(newPolicy: ZManaged[R0, E0, Policy[E]]): ZIO[R0, E0, UIO[Unit]] =
-        for {
-          newPolicyState     <- makeInUsePolicyState(scope, newPolicy)
-          // Atomically switch the policy and mark the old one as shutting down
-          currentPolicyState <- STM.atomically {
-                                  for {
-                                    oldState <- currentPolicy.get
-                                    _        <- oldState.shuttingDown.set(true)
-                                    _        <- currentPolicy.set(newPolicyState)
-                                  } yield oldState
-                                }.onInterrupt(newPolicyState.finalizer.apply(Exit.unit))
-          // From this point on, new policy calls will use the new policy
-          // Use a promise to decouple the 'await' effect from the fiber running the finalizer
-          policyReleased     <- Promise.make[Nothing, Any]
-          _                  <-
-            (currentPolicyState.shutdownComplete.await *> currentPolicyState.finalizer
-              .apply(Exit.unit)
-              .to(policyReleased)).fork
-        } yield policyReleased.await.unit
+      override def switch(newPolicy: ZManaged[R0, E0, Policy[E]], mode: Mode): ZIO[R0, E0, UIO[Unit]] =
+        mode match {
+          case Mode.Transition     =>
+            switchTransition(scope, currentPolicy, newPolicy)
+          case Mode.FinishInFlight =>
+            switchFinishInFlight(scope, currentPolicy, newPolicy)
+        }
     }
 
-  private case class InUsePolicyState[E](
+  private def switchTransition[E, E0, R0](
+    scope: ZManaged.Scope,
+    currentPolicy: TRef[PolicyState[E]],
+    newPolicy: ZManaged[R0, E0, Policy[E]]
+  ) =
+    for {
+      newPolicyState     <- makeInUsePolicyState(scope, newPolicy, awaitReady = UIO.unit)
+      // Atomically switch the policy and mark the old one as shutting down
+      currentPolicyState <- STM.atomically {
+                              for {
+                                oldState <- currentPolicy.get
+                                _        <- oldState.shuttingDown.set(true)
+                                _        <- currentPolicy.set(newPolicyState)
+                              } yield oldState
+                            }.onInterrupt(newPolicyState.finalizer.apply(Exit.unit))
+      // From this point on, new policy calls will use the new policy
+      // Use a promise to decouple the 'await' effect from the fiber running the finalizer
+      policyReleased     <- Promise.make[Nothing, Any]
+      _                  <-
+        (currentPolicyState.shutdownComplete.await *> currentPolicyState.finalizer
+          .apply(Exit.unit)
+          .to(policyReleased)).fork
+    } yield policyReleased.await.unit
+
+  private def switchFinishInFlight[E, E0, R0](
+    scope: ZManaged.Scope,
+    currentPolicy: TRef[PolicyState[E]],
+    newPolicy: ZManaged[R0, E0, Policy[E]]
+  ) =
+    for {
+      markAsReady        <- Promise.make[Nothing, Unit]
+      newPolicyState     <- makeInUsePolicyState(scope, newPolicy, markAsReady.await)
+      // Atomically switch the policy and mark the old one as shutting down
+      currentPolicyState <- STM.atomically {
+                              for {
+                                oldState <- currentPolicy.get
+                                _        <- oldState.shuttingDown.set(true)
+                              } yield oldState
+                            }.onInterrupt(newPolicyState.finalizer.apply(Exit.unit))
+      // From this point on, new policy calls will use the new policy but they have to
+      // wait for the old policy's calls to have finished
+      // Use a promise to decouple the 'await' effect from the fiber running the finalizer
+      policyReleased     <- Promise.make[Nothing, Any]
+      _                  <-
+        (currentPolicyState.shutdownComplete.await *>
+          markAsReady.completeWith(UIO.unit) *>
+          currentPolicyState.finalizer.apply(Exit.unit).to(policyReleased)).fork
+    } yield policyReleased.await.unit
+
+  private case class PolicyState[E](
     policy: Policy[E],
     finalizer: ZManaged.Finalizer,
     inFlightCalls: TRef[Long],
+    awaitReady: UIO[Unit],
     shuttingDown: TRef[Boolean],
     shutdownComplete: Promise[Nothing, Unit]
   )
 
   private def makeInUsePolicyState[R0, E0, E](
     scope: ZManaged.Scope,
-    newPolicy: ZManaged[R0, E0, Policy[E]]
-  ): ZIO[R0, E0, InUsePolicyState[E]] = for {
+    newPolicy: ZManaged[R0, E0, Policy[E]],
+    awaitReady: UIO[Unit]
+  ): ZIO[R0, E0, PolicyState[E]] = for {
     shutdownComplete   <- Promise.make[Nothing, Unit]
     inFlightCalls      <- TRef.make(0L).commit
     shuttingDown       <- TRef.make(false).commit
     r                  <- scope.apply(newPolicy)
     (finalizer, policy) = r
-  } yield InUsePolicyState(policy, finalizer, inFlightCalls, shuttingDown, shutdownComplete)
+  } yield PolicyState(policy, finalizer, inFlightCalls, awaitReady, shuttingDown, shutdownComplete)
 
 }
