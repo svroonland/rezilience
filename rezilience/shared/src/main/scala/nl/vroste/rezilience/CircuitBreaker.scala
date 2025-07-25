63af7733db270f497368a8ec4ac152a5344a83ec
Andrey S <norwegianwood409@gmail.com>
2025-07-25 19:41:34 +0500

    Prevent interruption of stateful HalfOpen logic

--- rezilience/shared/src/main/scala/nl/vroste/rezilience/CircuitBreaker.scala
+++ rezilience/shared/src/main/scala/nl/vroste/rezilience/CircuitBreaker.scala
@@ -295,19 +295,22 @@ object CircuitBreaker {
                           case Open     =>
                             ZIO.fail(CircuitBreakerOpen)
                           case HalfOpen =>
-                            for {
-                              isFirstCall <- halfOpenSwitch.getAndUpdate(_ => false)
-                              result      <- if (isFirstCall) {
-                                               tapZIOOnUserDefinedFailure(f)(
-                                                 onFailure = (strategy.shouldTrip(false) *> changeToOpen).uninterruptible,
-                                                 onSuccess = (changeToClosed *> strategy.onReset).uninterruptible
-                                               ).tapDefect(_ => (strategy.shouldTrip(false) *> changeToOpen).uninterruptible)
-                                                 .mapError(WrappedError(_))
-                                                 .onInterrupt(halfOpenSwitch.set(true))
-                                             } else {
-                                               ZIO.fail(CircuitBreakerOpen)
-                                             }
-                            } yield result
+                            ZIO.uninterruptibleMask { restore =>
+                              for {
+                                isFirstCall <- halfOpenSwitch.getAndUpdate(_ => false)
+                                result      <- if (isFirstCall) {
+                                                 tapZIOOnUserDefinedFailure(
+                                                   restore(f).onInterrupt(halfOpenSwitch.set(true))
+                                                 )(
+                                                   onFailure = strategy.shouldTrip(false) *> changeToOpen,
+                                                   onSuccess = changeToClosed *> strategy.onReset
+                                                 ).tapDefect(_ => strategy.shouldTrip(false) *> changeToOpen)
+                                                   .mapError(WrappedError(_))
+                                               } else {
+                                                 ZIO.fail(CircuitBreakerOpen)
+                                               }
+                              } yield result
+                            }
                         }
       } yield result)
         .tapBoth(
